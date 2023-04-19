9a4524b67abe82d49e64ee4d429ed97ee0f24ed5
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2023-04-19 15:57:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ function palindrome(str){
 	for(let i=str.length()-1;i>=0;i--){
 		rev=rev+str.charAt(i);
 	}
-	if(str.equals(rev))
+	if(str.equals(rev)){
 		return ans=true;
+	}
+	return ans;
 }
+
 module.exports = palindrome
